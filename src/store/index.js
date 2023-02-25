059dbac7cd20ddabfb4b7e5ceb242dce723ecf25
Nontobeko Mhlongo <ntobsie2010@gmail.com>
2023-02-25 15:52:59 +0200

    store

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,42 @@
 import { createStore } from 'vuex'
+import axios from 'axios';
+const bedUrl='https://awesomebed-mlis.onrender.com';
 
 export default createStore({
+  //commit mutation
   state: {
+    users:null,
+    user:null,
+    products:null,
+    product:null,
+    showSpinner:null,
+    message:null
   },
   getters: {
   },
+  //modify the state 
   mutations: {
+    setUsers(state,values){
+      state.user=values
+    },
+    setUser(state,value){
+      state.user=value
+    }
+
   },
+
   actions: {
+    async fetchUsers(context){
+      const res =await axios.get(`${bedUrl}Users`);
+      const {results,err}=await res.data
+      if(results){
+        context.commit('setUsers',results)
+      }else{
+        context.commit('setMessage',err)
+      }
+      
+
+    }
   },
   modules: {
   }
---
+++ src/views/adminView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container">
+    <div class="row">
+    <h1>
+      container
+    </h1>
+  </div>
+  <div class="row">
+    
+  </div>
+  </div>
+</template>
